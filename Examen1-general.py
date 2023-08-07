f8aa094bef832080dd97dcb2212aa0e07d75e251
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-08-07 16:09:19 -0500

    loops

--- Examen1-general.py
+++ Examen1-general.py
@@ -58,7 +58,7 @@ print(new_words)
 Tuplas y Operaciones:
 a) Crea una tupla con números enteros de tu elección y calcula su suma y su producto.
 b) Crea dos tuplas y combínalas para formar una nueva tupla.
-'''
+
 tuplas_1=(2,3,6,9)
 tuplas_2=(5,8,6,3,7,12)
 suma=0
@@ -69,11 +69,14 @@ print(f'la suma es igual a {suma}')
 
 new=(tuple(zip(tuplas_1,tuplas_2)))
 print(new)
+
+'''
+
 '''
 Diccionarios y Bucles:
 a) Crea un diccionario con nombres de países como claves y sus capitales como valores.
 b) Dada una lista de números, crea un diccionario donde las claves sean los números y los valores sean sus cuadrados.7
-'''
+
 #Empezando. 
 paises={
     'Colombia': 'Bogotá',
@@ -85,13 +88,15 @@ print(paises)
 numeros=[1,2,3,4,5,6]
 diccionario={k:k*2 for k in numeros}
 print(diccionario)
+'''
+
 
 
 '''
 Sets y Operaciones:
 a) Dada una lista de números duplicados, crea un set que contenga solo los valores únicos.
 b) Crea dos sets y encuentra la intersección y la unión de ambos.
-'''
+
 sets=[1,6,5,3,3,1,2,8,3,5]
 print(set(sets))
 
@@ -101,12 +106,15 @@ set_2={9,2,1,78,54,3,5,21}
 print(set_1|set_2)
 print(set_1 & set_2)
 
+'''
+
+
 
 '''
 Condicionales:
 a) Escribe un programa que tome un número y determine si es par o impar.
 b) Escribe un programa que tome una edad y determine si es un niño (menor de 12 años), adolescente (entre 12 y 18 años) o adulto.
-'''
+
 num=int(input(' digite el numero: '))
 if num %2==0 :
     print('par')
@@ -120,6 +128,23 @@ elif 12<edad>18:
     print('adolecente')
 else:
     print('adulto')
+
+'''
+
+'''
+Loops y Control de Flujo:
+a) Escribe un programa que muestre los números del 1 al 10 utilizando un bucle while.
+b) Escribe un programa que imprima los números pares del 1 al 20 utilizando un bucle for.
+
+'''
+for i in range (0,21,2):
+    print(i)
+
+num=1
+while num<11:
+    print(num)
+    num+=1
+    
 
 
 
